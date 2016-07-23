fad669c2495ea2813f56871ae4b6e2d13a8e3fb6
Luigi Grilli <luigi.grilli@gmail.com>
2016-07-23 11:48:34 +0100

    Printing version in deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -8,5 +8,5 @@ if($env:APPVEYOR_REPO_BRANCH -ne 'stable')
 Write-Host "Installing nuget package provider"
 Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
 
-Write-Host "Deploying module to powershellgallery"
-Publish-Module -Path .\Elastico -NuGetApiKey "$($env:powershellgallery_apikey)"
+Write-Host "Deploying module Elastico v$($env:APPVEYOR_BUILD_VERSION) to powershellgallery"
+Publish-Module -Path .\Elastico -NuGetApiKey "$($env:powershellgallery_apikey)" -Tags @("Elasticsearch")
