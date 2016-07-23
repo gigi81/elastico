4b93bc677a41eb8ae829e53e81290192bd320458
Luigi Grilli <luigi.grilli@gmail.com>
2016-07-23 10:34:16 +0100

    Improvements to deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,10 @@
 
 if($env:APPVEYOR_REPO_BRANCH -ne 'stable')
 {
-    Write-Host "Skipping deployment for current branch"
+    Write-Host "Skipping deployment for current branch '$($env:APPVEYOR_REPO_BRANCH)'"
     exit(0)
 }
 
+Write-Host "Deploying to powershellgallery"
 cd dist
 Publish-Module -Name Elastico -NuGetApiKey "$($env:powershellgallery_apikey)"
