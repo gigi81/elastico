44276c7d05fa927c37f0529ebb4b35f1157a7ca7
Luigi Grilli <luigi.grilli@gmail.com>
2016-07-23 13:33:50 +0100

    Improvements to manifest and deploy

--- deploy.ps1
+++ deploy.ps1
@@ -8,5 +8,7 @@ if($env:APPVEYOR_REPO_BRANCH -ne 'stable')
 Write-Host "Installing nuget package provider"
 Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
 
+Update-ModuleManifest -Path .\Elastico\Elastico.psd1 -ModuleVersion "$($env:APPVEYOR_BUILD_VERSION)"
+
 Write-Host "Deploying module Elastico v$($env:APPVEYOR_BUILD_VERSION) to powershellgallery"
-Publish-Module -Path .\Elastico -NuGetApiKey "$($env:powershellgallery_apikey)" -Tags @("Elasticsearch")
+Publish-Module -Path .\Elastico -NuGetApiKey "$($env:powershellgallery_apikey)"
