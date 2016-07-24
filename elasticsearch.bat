2ca054cab853ae7cd32117870da4fc5dd02dee23
Luigi Grilli <luigi.grilli@gmail.com>
2016-07-24 13:31:17 +0100

    Added elasticsearch service

---
+++ elasticsearch.bat
@@ -0,0 +1,29 @@
+@echo off
+rem ---------- tools
+SET CURL=tools\curl\bin\curl.exe
+SET ZIP=tools\7zip\7za.exe
+
+rem ---------- components versions
+SET ELASTIC_SEARCH_VERSION=2.3.2
+
+rem ---------- Download packages ----------
+if not exist "downloads" mkdir downloads
+
+if not exist "downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" %CURL% "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/%ELASTIC_SEARCH_VERSION%/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip
+rem --------------------------------------
+
+rem ---------- Unzip packages ----------
+rmdir /Q /S temp
+mkdir temp
+
+%ZIP% x -otemp downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip
+
+move temp\elasticsearch-%ELASTIC_SEARCH_VERSION% temp\elasticsearch
+rem ------------------------------------
+
+rem ---------- Install service ----------
+temp\elasticsearch\bin\service.bat install
+
+rem ---------- Start service ----------
+net start elasticsearch-service-x64
+
