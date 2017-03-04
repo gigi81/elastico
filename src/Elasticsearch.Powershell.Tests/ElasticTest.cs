41cad661b03acc1471a80bfbc0faf3d11fe6f1e0
Luigi Grilli <luigi.grilli@gmail.com>
2017-03-04 09:11:04 +0000

    Trying to fix V5 wait

--- src/Elasticsearch.Powershell.Tests/ElasticTest.cs
+++ src/Elasticsearch.Powershell.Tests/ElasticTest.cs
@@ -20,7 +20,7 @@ namespace Elasticsearch.Powershell.Tests
 
 #if ESV5
             //block until es is ready
-            _server.Ready().RunSynchronously();
+            _server.Ready().GetAwaiter().GetResult();
 #endif
         }
 
