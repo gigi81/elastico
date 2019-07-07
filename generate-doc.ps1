c03780af9e549e899481105a69ea5756a6dbb0d8
Luigi Grilli <luigi.grilli@gmail.com>
2019-07-07 09:01:46 +0100

    Added psDoc as a submodule

--- generate-doc.ps1
+++ generate-doc.ps1
@@ -1,8 +1,3 @@
-if(!Test-Path psDoc)
-{
-    git clone https://github.com/ChaseFlorell/psDoc.git 2> $null
-}
-
 Import-Module ".\Elastico\Elastico.psd1"
 
 Get-Command -Module Elastico | Format-Table -Property Name,Version
