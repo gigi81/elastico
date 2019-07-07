501e08ac3f9db99dfb3c8281fbbd30739be2fc8e
Luigi Grilli <luigi.grilli@gmail.com>
2019-07-07 09:27:42 +0100

    Updated deploy script to support azure devops

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,20 @@
-
-if($env:APPVEYOR_REPO_BRANCH -ne 'stable')
-{
-    Write-Host "Skipping deployment for current branch '$($env:APPVEYOR_REPO_BRANCH)'"
-    exit(0)
-}
-
 Write-Host "Installing nuget package provider"
 Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
 
+Write-Host "Importing module"
 Import-Module ".\Elastico\Elastico.psd1"
 $cmdlets = (Get-Command -Module Elastico).Name
 
-Write-Host "Updating Manifest"
-Update-ModuleManifest -Path .\Elastico\Elastico.psd1 -ModuleVersion "$($env:APPVEYOR_BUILD_VERSION)"
+if(-not [string]::IsNullOrEmpty($env:BUILD_BUILDNUMBER)){ $version = $env:BUILD_BUILDNUMBER }
+elseif(-not [string]::IsNullOrEmpty($env:APPVEYOR_BUILD_VERSION)) { $version = $env:APPVEYOR_BUILD_VERSION }
+else { $version = '1.0.0' }
+
+Write-Host "Updating Manifest $version"
+Update-ModuleManifest -Path .\Elastico\Elastico.psd1 -ModuleVersion $version
 Update-ModuleManifest -Path .\Elastico\Elastico.psd1 -CmdletsToExport $cmdlets
 
-Write-Host "Deploying module Elastico v$($env:APPVEYOR_BUILD_VERSION) to powershellgallery"
-Publish-Module -Path .\Elastico -NuGetApiKey "$($env:powershellgallery_apikey)"
+if($env:APPVEYOR_REPO_BRANCH -eq 'stable' -or $env:BUILD_SOURCEBRANCHNAME -eq 'stable')
+{
+    Write-Host "Deploying module Elastico v$($env:APPVEYOR_BUILD_VERSION) to powershellgallery"
+    Publish-Module -Path .\Elastico -NuGetApiKey "$($env:powershellgallery_apikey)"
+}
